fd3361d2097f80841c709a86927b7ada6840d65c
youshamahmood <youshamahmood96@gmail.com>
2020-09-26 18:42:59 +0600

    practise

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
 
+app.use(cors());
+app.use(bodyParser.json());
 
 
 app.get('/',(req,res)=>{
-    res.send('I know how to open node yess')
+    const fruit={
+        product:'ada',
+        price:220 
+    }
+    res.send(fruit)
 })
-app.listen(4200,()=>console.log('Listening to port 3000'))
+
+
+const users =['Asad','Moin','Saber','susmita','sohana','sabana']
+
+//get
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id]
+    res.send({id,name});
+    })
+
+//post
+app.post('/addUser',(req, res)=>{
+    //save to database
+    const user = req.body
+    user.id = 55;
+    res.send(user);
+})
+
+app.listen(4200,()=>console.log('Listening to port 4200'))
